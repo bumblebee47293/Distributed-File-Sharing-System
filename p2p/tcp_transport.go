865321ce85e21ecd32e26b014c9e435bb357d6fb
Vasil <bumblebee47293@gmail.com>
2024-04-16 03:55:33 +0530

    feat: add a struct for TCPPeer struct

--- p2p/tcp_transport.go
+++ p2p/tcp_transport.go
@@ -6,6 +6,15 @@ import (
 	"sync"
 )
 
+// TCPPeer represents the remote node over a TCP established connection.
+type TCPPeer struct {
+	// conn is the underlying connection of the peer
+	conn net.Conn
+	// if we dial and retrieve a conn => outbound ==  true
+	// if we accept and retrieve a conn => outbound == false
+	outbound bool
+}
+
 type TCPTransport struct {
 	listenAddress string
 	listener      net.Listener
